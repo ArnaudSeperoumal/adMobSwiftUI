aa37364673459ad6eb482c2d4f71aba17fb36de4
Arnaud SEPEROUMAL <arnaudseperoumal@gmail.com>
2025-04-11 00:35:56 +0200

    try to fix issue

--- Package.swift
+++ Package.swift
@@ -1,38 +1,27 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "adMobSwiftUI",
     platforms: [
-        .iOS(.v18)
+        .iOS(.v17) // v18 pourrait ne pas être supporté par Xcode 16.2, reste sur v17 pour l'instant
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "adMobSwiftUI", targets: ["adMobSwiftUI"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "adMobSwiftUI",
             dependencies: [
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
-            ],
-            path: "adMobSwiftUI/Sources/adMobSwiftUI",
-            exclude: []
+            ]
         ),
         .testTarget(
             name: "adMobSwiftUITests",
             dependencies: ["adMobSwiftUI"]
-            path: "adMobSwiftUI/Tests/adMobSwiftUITests",
-            exclude: []
         )
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
