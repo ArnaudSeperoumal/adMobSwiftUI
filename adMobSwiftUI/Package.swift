86a6de9cea68488c97759128415c5ab36d25d2a8
Arnaud SEPEROUMAL <arnaudseperoumal@gmail.com>
2025-04-11 01:04:10 +0200

    Create Package.swift

---
+++ adMobSwiftUI/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "adMobSwiftUI",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "adMobSwiftUI",
+            targets: ["adMobSwiftUI"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "adMobSwiftUI",
+            dependencies: [
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+            ]
+        ),
+        .testTarget(
+            name: "adMobSwiftUITests",
+            dependencies: ["adMobSwiftUI"]
+        ),
+    ]
+)
